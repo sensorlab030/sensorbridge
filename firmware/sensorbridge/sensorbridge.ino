f4e3faf1a0418c78eb9f3de1d7fc2a50d653e0b4
Wilco Tomassen <wilco@cleverfranke.com>
2017-05-03 15:09:13 +0200

    Added new initial firmware, reading the analog inputs, as well as the digital inputs

---
+++ firmware/sensorbridge/sensorbridge.ino
@@ -0,0 +1,48 @@
+
+#define SENSOR_COUNT 6
+#define STATUS_LED_PIN 7
+
+// Mapping of sensors to pins. The first value of the sensor pin arrays
+// is the pin that Sensor1 is connected to, second value for Sensor2 and
+// so on.
+//                       Sensor id: 1, 2, 3, 4, 5, 6
+uint8_t sensorDataPins[]        = { 5, 4, 3, 2, 0, 1 }; // Analog input pin the sensor is connected to
+uint8_t sensorConnectionPins[]  = { 4, 5, 3, 2, 1, 0 }; // Digital input pin the sensor is connected to
+
+void setup() {
+
+  // Setup pin modes for the connection pins
+  for (int i = 0; i < SENSOR_COUNT; i++) {
+    pinMode(sensorConnectionPins[i], INPUT);
+  }
+  pinMode(STATUS_LED_PIN, OUTPUT);
+
+  // Start serial communication
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+
+  // Read and communicate values
+  for (int i = 0; i < SENSOR_COUNT; i++) {
+
+    // Determine value of pin
+    bool sensorConnected = digitalRead(sensorConnectionPins[i]);
+    uint16_t value = sensorConnected ? analogRead(sensorDataPins[i]) : 65535;
+
+    // Write value to serial port
+    Serial.write(highByte(value));
+    Serial.write(lowByte(value));
+    
+  }
+  
+  // Terminate series of values
+  Serial.write(0x0D);
+  Serial.write(0x0A);
+
+  // Wait for a bit
+  delay(20);
+  
+}
+
