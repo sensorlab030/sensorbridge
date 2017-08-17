61ebb6aa8f4d4fea9e817bb5e9871681715a4484
Wilco Tomassen <wilco@cleverfranke.com>
2017-08-17 14:10:52 +0200

    Updated firmware to three sensors, added animation

--- firmware/sensorbridge/sensorbridge.ino
+++ firmware/sensorbridge/sensorbridge.ino
@@ -1,21 +1,30 @@
 
-#define SENSOR_COUNT 6
-#define STATUS_LED_PIN 7
+#define SENSOR_COUNT 3
 
 // Mapping of sensors to pins. The first value of the sensor pin arrays
 // is the pin that Sensor1 is connected to, second value for Sensor2 and
 // so on.
-//                       Sensor id: 1, 2, 3, 4, 5, 6
-uint8_t sensorDataPins[]        = { 5, 4, 3, 2, 0, 1 }; // Analog input pin the sensor is connected to
-uint8_t sensorConnectionPins[]  = { 4, 5, 6, 7, 3, 2 }; // Digital input pin the sensor is connected to
+//                Sensor id: 1, 2, 3
+uint8_t sensorDataPins[] = { 5, 4, 3 }; // Analog input pin the sensor is connected to to collect data
+uint8_t sensorConnPins[] = { 2, 3, 4 }; // Digital input pin the sensor is connected to check connection (uses internal pullup)
+uint8_t sensorLedPins[]  = { 5, 6, 7 }; // Digital input pin the sensor is connected to display connection status
 
 void setup() {
 
-  // Setup pin modes for the connection pins
-  for (int i = 0; i < SENSOR_COUNT; i++) {
-    pinMode(sensorConnectionPins[i], INPUT);
+  // Setup pin modes for the digital pins
+  for (int i = 0; i < SENSOR_COUNT; i++) {    
+    pinMode(sensorConnPins[i], INPUT_PULLUP);
+    pinMode(sensorLedPins[i], OUTPUT);
   }
-  pinMode(STATUS_LED_PIN, OUTPUT);
+
+  // Booting LED animation 
+  for (int i = 0; i < 40; i++) {
+    int enabledLedIndex = i % SENSOR_COUNT;
+    for (int j = 0; j < SENSOR_COUNT; j++) {    
+      digitalWrite(sensorLedPins[j], j == enabledLedIndex);
+    }
+    delay(50);
+  }
 
   // Start serial communication
   Serial.begin(9600);
@@ -28,7 +37,8 @@ void loop() {
   for (int i = 0; i < SENSOR_COUNT; i++) {
 
     // Determine value of pin
-    bool sensorConnected = digitalRead(sensorConnectionPins[i]);
+    bool sensorConnected = (digitalRead(sensorConnPins[i]) == LOW); // Pin is pulled low if sensor is connected
+    digitalWrite(sensorLedPins[i], sensorConnected);
     uint16_t value = sensorConnected ? analogRead(sensorDataPins[i]) : 65535;
 
     // Write value to serial port
